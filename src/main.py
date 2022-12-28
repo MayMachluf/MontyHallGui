db353ff16b598cb46c8c599bed395813010644e4
Liav <regev04@gmail.com>
2022-12-28 15:40:22 +0200

    update

--- src/main.py
+++ src/main.py
@@ -2,163 +2,200 @@ import tkinter as tk
 from tkinter import messagebox
 from PIL import ImageTk, Image
 import random
+from mhProblemGp import *
+from graph import *
+import pandas as pd
+import plotly.express as px
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
 import time
 
-num_doors = 0
+door_amount = 0
+door_list = {}
 game_stage = 0
-result = {}
-games = 0
+score = {'win': 0, 'loss': 0}
+game_count = 0
 
 
-def show_msg():
-    n = doorNumberEntry.get()
-    if n.isnumeric():
-        if int(n) in range(3, 10):
-            label.destroy()
-            doorNumberEntry.destroy()
-            btn.destroy()
-            func(int(n))
-            return
+def check_input():
+    global door_amount
 
-        tk.messagebox.showerror("error", "your number should be in range 3-9")
+    if not entry.get().isnumeric():
+        tk.messagebox.showerror("error", "please enter number in digits")
         return
-    tk.messagebox.showerror("error", "please enter number in digits")
+
+    if int(entry.get()) < 3 or int(entry.get()) > 200:
+        tk.messagebox.showerror("error", "your number should be in range 3-200")
+        return
+
+    if door_amount != 0 and (door_amount != int(entry.get())):
+        answer = tk.messagebox.askyesno(title="Warning",
+                                        message="If you change your amount of doors your score will reset.\n"
+                                                "Do you wish to continue?")
+        if not answer:
+            return
+        else:
+            reset_score()
+
+    door_amount = int(entry.get())
+    start_game()
 
 
-doorList = {}
+def start_game():
+    entry.pack_forget()
+    lbl.pack_forget()
+    btn.pack_forget()
+
+    if door_amount in range(3, 7):
+        show_doors()
+
+    else:
+        res = run_mh(door_amount)
+        show_graph(res)
 
 
-def func(n: int):
-    doorClosedImage = ImageTk.PhotoImage(Image.open("images/door_closed.png"))
+def show_graph(res):
+    frame.pack_forget()
+    doorFrame.pack_forget()
+    midGameFrame.pack_forget()
 
-    if n == 3 or n == 4:
-        for i in range(n):
-            door1 = tk.Button(image=doorClosedImage, text=str(i), font=('Helvetica 15 bold'), compound=tk.LEFT,
-                              command=lambda x=i: door_pick(x))
-            door1.image = doorClosedImage
-            door1.grid(row=2, column=i + 2)
-            doorList[door1] = "goat"
+    graphFrame.pack()
 
-    if n == 5 or n == 7 or n == 9:
-        k = 0
-        j = 0
-        for i in range(n):
-            door1 = tk.Button(image=doorClosedImage, text=str(i), font=('Helvetica 15 bold'), compound=tk.LEFT,
-                              command=lambda x=i: door_pick(x))
-            door1.image = doorClosedImage
-            doorList[door1] = "goat"
+    tk.Label(graphFrame, text=f"number of partitions: {res[0]}").pack()
+    tk.Label(graphFrame, text=f"number of games: {res[1]}").pack()
+    tk.Label(graphFrame, text=f"number of wins because of choice change: {res[2]}").pack()
+    tk.Label(graphFrame, text=f"number of losses because of choice change: {res[3]}").pack()
+    tk.Label(graphFrame, text=f"number of choice change without influence: {res[4]}").pack()
 
-            if i < (n / 2):
-                door1.grid(row=2, column=j)
-                j += 2
+    fig = Figure(figsize=(10, 8))
+    ax = fig.add_subplot(111)
 
-            else:
-                door1.grid(row=3, column=k + 1)
-                k += 2
+    ax.pie([res[2], res[3], res[4]],
+           radius=1,
+           labels=['wins after choice change',
+                   'losses after choice change',
+                   'No influence'],
+           autopct='%0.2f%%',
+           shadow=True)
 
-    if n == 6 or n == 8:
-        k = 0
-        j = 0
-        for i in range(n):
-            door1 = tk.Button(image=doorClosedImage, text=str(i), font=('Helvetica 15 bold'), compound=tk.LEFT,
-                              command=lambda x=i: door_pick(x))
-            door1.image = doorClosedImage
-            doorList[door1] = "goat"
+    chart1 = FigureCanvasTkAgg(fig, graphFrame)
+    chart1.get_tk_widget().pack(pady=10)
 
-            if i < n / 2:
-                door1.grid(row=2, column=j)
-                j += 2
-
-            else:
-                door1.grid(row=3, column=k)
-                k += 2
-
-    key, val = random.choice(list(doorList.items()))
-    doorList[key] = 'car'
+    score['win'] = score['win'] + int(res[2])
+    score['loss'] = score['loss'] + (int(res[1]) - int(res[2]))
+    refresh_score()
 
 
-def door_pick(i):
+def show_doors():
+    door_closed_image = ImageTk.PhotoImage(Image.open("images/door_closed.png"))
+
+    for i in range(door_amount):
+        door = tk.Button(doorFrame,
+                         text=str(i + 1),
+                         image=door_closed_image,
+                         font='Helvetica 15 bold',
+                         compound=tk.LEFT,
+                         command=lambda x=i: door_pick(x))
+        door.image = door_closed_image
+        door.pack(side='left', expand=1)
+        door_list[door] = "goat"
+
+    key, val = random.choice(list(door_list.items()))
+    door_list[key] = 'car'
+
+
+def door_pick(door):
     global game_stage
+    global game_count
+    pick = list(door_list.keys())[door]
 
     if game_stage == 0:
+        midGameFrame.pack()
+        lbl = tk.Label(midGameFrame,
+                       text="The presenter has revealed one of the goats for you, would you like to change your pick?")
+        lbl.pack(pady=20)
+        btnYes = tk.Button(midGameFrame, text="Yes")
+        btnNo = tk.Button(midGameFrame, text="No")
+        btnYes['command'] = lambda: yes_btn()
+        btnNo['command'] = lambda: no_btn(door)
+        btnYes.pack()
+        btnNo.pack()
+
         while True:
-            key, val = random.choice(list(doorList.items()))
-            if val != 'car' and list(doorList.keys())[i] != key:
-                game_stage += 1
-                change_pic(key)
-                yesBtn = tk.Button(text="Yes")
-                noBtn = tk.Button(text="No")
-                lbl = tk.Label(text="Would you like to change your choice?")
-                yesBtn['command'] = lambda: yes_btn(yesBtn, noBtn, lbl)
-                noBtn['command'] = lambda: no_btn(yesBtn, noBtn, lbl, list(doorList.keys())[i])
+            key, val = random.choice(list(door_list.items()))
+            if key != pick and val != 'car':
+                break
 
-                x = len(doorList)
-                lbl.grid(row=4, column=int(x / 2))
-                yesBtn.grid(row=5, columnspan=int(x / 2))
-                noBtn.grid(row=5, columnspan=int(x / 2 + 1))
-
-                return
+        change_picture(key)
 
     if game_stage == 1:
-        key = list(doorList.keys())[i]
-        change_pic(key)
-
-        if doorList.get(key) == 'goat':
-            for key, value in doorList.items():
+        change_picture(pick)
+        if door_list.get(pick) == 'goat':
+            for key, value in door_list.items():
                 if value == 'car':
-                    change_pic(key)
+                    change_picture(key)
 
-        game_stage = 2
-        if doorList.get(key) == 'goat':
-            result[games] = False
+            lbl = tk.Label(frame, text="You were wrong!")
+            lbl.pack(pady=30)
+            score['loss'] = score['loss'] + 1
+            game_count += 1
+
         else:
-            result[games] = True
+            lbl = tk.Label(frame, text="You were right!")
+            lbl.pack(pady=30)
+            score['win'] = score['win'] + 1
+            game_count += 1
 
-        run_again()
-
-        return
+        againButton = tk.Button(midGameFrame, text="Try again", command=lambda: restart(True))
+        againButton.pack(pady=30)
+        game_stage = 3
 
     if game_stage == 2:
+        change_picture(pick)
+
+        if door_list.get(pick) == 'goat':
+            for key, value in door_list.items():
+                if value == 'car':
+                    change_picture(key)
+                    lbl = tk.Label(midGameFrame, text="You were wrong!")
+                    lbl.pack(pady=30)
+                    score['loss'] = score['loss'] + 1
+                    game_count += 1
+                    break
+
+        else:
+            lbl = tk.Label(frame, text="You were right!")
+            lbl.pack(pady=30)
+            score['win'] = score['win'] + 1
+            game_count += 1
+
+        againButton = tk.Button(midGameFrame, text="Try again", command=lambda: restart(True))
+        againButton.pack(pady=30)
+        game_stage = 3
+
+    if game_stage == 3:
         return
 
 
-def run_again():
-    againBtn = tk.Button(text="Would you like to run again?")
-    againBtn['command'] = lambda: reset(againBtn)
-    againBtn.grid(row=3, columnspan=1)
-
-
-def reset(againBtn):
+def yes_btn():
     global game_stage
-    game_stage = 0
-    againBtn.destroy()
-    for x in doorList:
-        x.destroy()
-
-    game_start()
-
-
-def yes_btn(*args):
-    global game_stage
-    for i in args:
-        i.destroy()
+    for i in midGameFrame.winfo_children():
+        i.pack_forget()
 
     game_stage = 1
 
 
-def no_btn(btn1, btn2, lbl, door):
+def no_btn(door):
     global game_stage
-    btn1.destroy()
-    btn2.destroy()
-    lbl.destroy()
-
-    change_pic(door)
+    for i in midGameFrame.winfo_children():
+        i.pack_forget()
 
     game_stage = 2
+    door_pick(door)
 
 
-def change_pic(labelname):
-    if doorList.get(labelname) == 'goat':
+def change_picture(labelname):
+    if door_list.get(labelname) == 'goat':
         photo1 = ImageTk.PhotoImage(Image.open("images/door_goat.png"))
         labelname.configure(image=photo1)
         labelname.photo = photo1
@@ -170,19 +207,90 @@ def change_pic(labelname):
 
 
 window = tk.Tk()
+doorNumberEntry = tk.Entry(window)
+
+
+# IMAGE
+# img = ImageTk.PhotoImage(Image.open("images/background.jpg"))
+# label1 = tk.Label(window, image=img)
+# label1.place(x=0, y=0)
+
+
+def start(skip_check=False):
+    refresh_score()
+    frame.pack()
+    doorFrame.pack()
+    lbl.pack(pady=5)
+    entry.pack(pady=5)
+    btn.pack()
+
+    if skip_check:
+        start_game()
+
+
+def restart(skip_check=False):
+    global game_stage, door_list, door_amount
+    door_list = {}
+    game_stage = 0
+
+    for i in frame.winfo_children():
+        i.pack_forget()
+
+    for x in doorFrame.winfo_children():
+        x.pack_forget()
+
+    for x in midGameFrame.winfo_children():
+        x.pack_forget()
+
+    for x in graphFrame.winfo_children():
+        x.pack_forget()
+
+    frame.pack_forget()
+    doorFrame.pack_forget()
+    midGameFrame.pack_forget()
+    graphFrame.pack_forget()
+
+    start(skip_check)
+
+
+def refresh_score():
+    if score['win'] == 0 and score['loss'] == 0:
+        statsLbl['text'] = f"No games played yet."
+
+    else:
+        statsLbl['text'] = f"Score: {score['win']} Wins, {score['loss']} Losses"
+
+
+def reset_score():
+    global score, door_amount
+    score = {'win': 0, 'loss': 0}
+    door_amount = 0
+    refresh_score()
+    restart()
+
+
 frame = tk.Frame()
-doorNumberEntry = tk.Entry(frame)
-label = tk.Label(frame, text="Please enter number of doors here")
-btn = tk.Button(frame, text="Confirm", command=show_msg)
+doorFrame = tk.Frame()
+graphFrame = tk.Frame()
+midGameFrame = tk.Frame()
+lbl = tk.Label(frame, text="Please enter number of doors here")
+entry = tk.Entry(frame)
+btn = tk.Button(frame, text="Confirm", command=check_input)
 
+bottomFrame = tk.Frame()
+bottomFrame.pack(side=tk.BOTTOM)
 
-def game_start():
-    label.grid(rowspan=1, columnspan=1)
-    doorNumberEntry.grid(rowspan=1, columnspan=2)
-    btn.grid(rowspan=2, columnspan=1)
+btnRestart = tk.Button(bottomFrame, text="Restart Game", command=restart)
+btnRestart.pack(side=tk.LEFT, padx=5, pady=10, expand=1)
+resetScore = tk.Button(bottomFrame, text="Reset Score", command=reset_score)
+resetScore.pack(side=tk.LEFT, padx=5, pady=10, expand=1)
+btnStats = tk.Button(bottomFrame, text="Show Statistics")
+btnStats.pack(side=tk.LEFT, padx=5, pady=10, expand=1)
 
+statsLbl = tk.Label(window, text=f"Score: {score}")
+statsLbl.pack(side=tk.BOTTOM, pady=10)
 
-game_start()
+start()
 
 window.geometry("1100x700")
 window.mainloop()
